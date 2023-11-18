0b51b666bf51b0801756f55d7d700fdda919db55
pityke <ballllaistvan@gmail.com>
2023-11-18 13:30:02 +0100

    finishing post method implementation with exception handling

--- src/main/java/com/example/student/StudentService.java
+++ src/main/java/com/example/student/StudentService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 
 
 import java.util.List;
+import java.util.Optional;
+
 //This needs to be a SpringBean to provide the dependency injection
 @Service
 public class StudentService {
@@ -19,7 +21,12 @@ public class StudentService {
     return studentRepository.findAll();
   }
   public void addNewStudent(Student student){
+  Optional<Student>studentByEmail = studentRepository
+          .findStudentByEmail(student.getEmail());
     System.out.println(student);
-
+    if(studentByEmail.isPresent()){
+      throw new IllegalStateException("email is taken");
+    }
+  studentRepository.save(student);
   }
 }
